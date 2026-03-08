79bd89ec6d7948fa900ab9589840f00689181e6e
Abba Abdel djabbar <aa.abba@ensta.edu.dz>
2026-03-08 12:45:49 +0100

    project structure

--- entities/administrator.py
+++
@@ -1,7 +0,0 @@
-from crypto.rsa_utils import rsa_sign, rsa_verify
-
-def blind_sign(masked_message: int, private_key: tuple) -> int:
-    return rsa_sign(masked_message, private_key)
-
-def verify_signature(message: int, signature: int, public_key: tuple) -> bool:
-    return rsa_verify(message, signature, public_key)
